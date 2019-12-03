ba8c4499b55c5d0ce897f403a2def0e49b5ca898
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-12-03 19:32:06 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,7 +25,7 @@ end
 def initial_round
   #deal_card
   #deal_card
-  puts display_card_total(deal_card)
+  #puts display_card_total(deal_card)
   return display_card_total(deal_card)
   # code #initial_round here
 end
