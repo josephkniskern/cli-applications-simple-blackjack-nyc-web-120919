c817227751cb3fc15fb0a98759c0127cfa8f13ba
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-12-17 22:19:26 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,17 +30,23 @@ def initial_round
   return card_total
 end
 
-def hit?
+def hit?(card_total)
   prompt_user
   result = get_user_input
-  if result == "h"
+  if result == 'h'
     card_total += deal_card
+  elsif result == 's'
+    card_total
+  else
+    invalid_command
   end
   card_total
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+  get_user_input
+  prompt_user
 end
 
 #####################################################
